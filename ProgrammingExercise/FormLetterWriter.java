05b8de11b4d76ba872b2dd376907242a3e63e96c
tianhui-shi <tianhui.shi@my.jcu.edu.au>
2023-01-20 14:24:03 +0800

    all done 1

---
+++ ProgrammingExercise/FormLetterWriter.java
@@ -0,0 +1,20 @@
+public class FormLetterWriter
+{
+    public static void main(String[] args)
+    {
+
+    }
+    public static void displaySalutation(String name)
+    {
+        System.out.println("Dear" + name);
+    }
+
+    public static void displaySalutation(String firstName, String lastName)
+    {
+        System.out.println("Dear " + firstName + " " + lastName);
+    }
+    public static void displayLetter()
+    {
+        System.out.println(" Thank for your recent order. \n");
+    }
+}
